298d2c69ec5084eb89e57a9917afc697117e6d08
zipang <christophe.desguez@gmail.com>
2025-02-07 21:57:34 +0100

    Utilities : parse the URL parameters

---
+++ src/lib/parameters.ts
@@ -0,0 +1,35 @@
+/**
+ * Extract the parameters from the page URL
+ *
+ * Example URL:
+ *
+ * `your-page.html?theme=dark&mode=preview&optionalParam=value`
+ */
+
+import { type EditorMode } from "../editor/MarkdownEditor";
+
+export interface PageParameters extends EditorMode {
+  theme: "default" | "dracula" | "fruity";
+}
+export const DEFAULTS_PARAMS: PageParameters = {
+  theme: "default",
+  mode: "edit",
+};
+
+/**
+ * Retrieves URL parameters and provides default values.
+ *
+ * @param {object} defaults - An object containing default parameter values.
+ * @returns {object} An object containing the URL parameters,
+ *                   merged with the default values.
+ */
+export function getPageParameters() {
+  const urlParams = new URLSearchParams(window.location.search);
+  const params = { ...DEFAULTS_PARAMS }; // Create a copy of defaultParams
+
+  for (const [key, value] of urlParams.entries()) {
+    params[key] = value;
+  }
+
+  return params;
+}
